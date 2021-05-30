a487aaa143717512c71fc5265df643fdea8aa8c7
Dan Eliahu <daneliyahu@cs.huji.ac.il>
2021-05-30 14:15:20 +0300

    18 - Multiple Audio Clips

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] private float levelLoadDelay = 2f;
+    [SerializeField] private AudioClip crashSfx;
+    [SerializeField] private AudioClip successSfx;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                print("Friendly");
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartCrashSequence();
+                break;
+        }
+    }
+
+    private void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        _audioSource.Stop();
+        _audioSource.PlayOneShot(crashSfx);
+        Invoke(nameof(ReloadLevel), levelLoadDelay);
+    }
+
+    private void StartSuccessSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        _audioSource.Stop();
+        _audioSource.PlayOneShot(successSfx);
+        Invoke(nameof(LoadNextLevel), levelLoadDelay);
+    }
+
+    private void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void LoadNextLevel()
+    {
+        var nextSceneIndex = (SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float thrust = 10f;
+    [SerializeField] private float torque = 10f;
+    [SerializeField] private AudioClip thrustSfx;
+    private Rigidbody _rigidbody;
+    private AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidbody.AddRelativeForce(Vector3.up * thrust);
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.PlayOneShot(thrustSfx);
+            }
+        }
+        else
+        {
+            _audioSource.Stop();
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            _rigidbody.AddRelativeTorque(Vector3.forward * torque,ForceMode.Impulse);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _rigidbody.AddRelativeTorque(Vector3.back * torque,ForceMode.Impulse);
+        }
+    }
+}
